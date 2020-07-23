ed4694ab6c5358ee15bd5abc1fd6099e37ce3522
maxiTD <maxi.torresdiaz@gmail.com>
2020-07-23 14:14:03 -0300

    .

---
+++ include/funciones/filtar.php
@@ -0,0 +1,28 @@
+<?php
+    function obtenerDatos() {
+        //Impotar la conexion a la DB
+        include('../demo_formulario/include/funciones/db.php');
+
+        return $conn->query("SELECT nombre, apellido, dni, edad, fecha_nacimiento, genero, pais, provincia, localidad, calle, numero FROM personas WHERE (edad > 24 && edad < 35)");
+    }
+
+    $personas = obtenerDatos();
+    //si num_rows no es NULL es porque hay registros
+    if ($personas->num_rows) {
+        foreach ($personas as $persona) { ?>
+            <tr>
+                <td><?php echo $persona['nombre']; ?></td>
+                <td><?php echo $persona['apellido']; ?></td>
+                <td><?php echo $persona['dni']; ?></td>
+                <td><?php echo $persona['edad']; ?></td>
+                <td><?php echo $persona['fecha_nacimiento']; ?></td>
+                <td><?php echo $persona['genero']; ?></td>
+                <td><?php echo $persona['pais']; ?></td>
+                <td><?php echo $persona['provincia']; ?></td>
+                <td><?php echo $persona['localidad']; ?></td>
+                <td><?php echo $persona['calle']; ?></td>
+                <td><?php echo $persona['numero']; ?></td>
+            </tr>
+    <?php }
+    }
+?>
--- include/layout/tabla-resultados.php
+++ include/layout/tabla-resultados.php
@@ -17,36 +17,7 @@
             </tr>
         </thead>
         <tbody>
-
-            <?php
-                //include_once ('../funciones/filtrar.php');
-
-                function obtenerDatos() {
-                    //Impotar la conexion a la DB
-                    include('../funciones/db.php');
-
-                    return $conn->query("SELECT nombre, apellido, dni, edad, fecha_nacimiento, genero, pais, provincia, localidad, calle, numero FROM personas WHERE (edad > 24 && edad < 35)");
-                }
-
-                $personas = obtenerDatos();
-                //si num_rows no es NULL es porque hay registros
-                if ($personas->num_rows) {
-                    foreach ($personas as $persona) { ?>
-                        <tr>
-                            <td><?php echo $persona['nombre']; ?></td>
-                            <td><?php echo $persona['apellido']; ?></td>
-                            <td><?php echo $persona['dni']; ?></td>
-                            <td><?php echo $persona['edad']; ?></td>
-                            <td><?php echo $persona['fecha_nacimiento']; ?></td>
-                            <td><?php echo $persona['genero']; ?></td>
-                            <td><?php echo $persona['pais']; ?></td>
-                            <td><?php echo $persona['provincia']; ?></td>
-                            <td><?php echo $persona['localidad']; ?></td>
-                            <td><?php echo $persona['calle']; ?></td>
-                            <td><?php echo $persona['numero']; ?></td>
-                        </tr>
-                <?php }
-                }?>
+            <?php include_once ('../demo_formulario/include/funciones/filtar.php'); ?>
         </tbody>
     </table>
 </div>
